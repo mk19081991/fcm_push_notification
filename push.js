a4cd7a5a563fbf13a700704a65a9623fc87ec451
wegile <nitish@wegile.com>
2021-10-25 12:36:49 +0530

    Push notification on 25-10-2021

---
+++ push.js
@@ -0,0 +1,86 @@
+var express = require('express');
+const fetch = require('node-fetch');
+const { google } = require('googleapis');
+var app = express();
+
+
+function getAccessToken() {
+    return new Promise(function(resolve, reject) {
+        //const key = require('./talk-it-cad9e-firebase-adminsdk-szofl-75fc1114a2.json');
+        const jwtClient = new google.auth.JWT(
+            key.client_email,
+            null,
+            key.private_key,
+            ['https://www.googleapis.com/auth/firebase.messaging'],
+            null
+        );
+        jwtClient.authorize(function(err, tokens) {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve(tokens.access_token);
+        });
+    });
+}
+
+
+var server = app.listen(8085, function() {
+    console.log("Server is running on port 8085");
+});
+
+app.get('/', (req, res) => res.send('Hello this nodejs server. Enjoy :)'));
+
+
+app.get('/sendNotification', function(req, res) {
+    getAccessToken().then(function(accessToken) {
+        console.log("signedAccessToken", accessToken);
+        let title="Hi test";
+        let bodyMessage="Hi test";
+        var options = {
+            "message": {
+                "token": "*******************************************************************************",
+                "notification": {
+                    "title": title,
+                    "body": bodyMessage
+                },
+                "data": {
+                    "title": title,
+                    "body": bodyMessage
+                },
+                "android": {
+                    "notification": {
+                        "click_action": "android.intent.action.MAIN"
+                    }
+                },
+                "apns": {
+                    "headers": {
+                        "priority": "high"
+                    },
+                    "payload": {
+                        "aps": {
+                            "badge": 1
+                        },
+                        "content_available": 1,
+                        "data": {
+                            "title": title,
+                            "body": bodyMessage
+                        }
+                    }
+                }
+            }
+        };
+
+        fetch('https://fcm.googleapis.com/v1/projects/talk-it-cad9e/messages:send', {
+                method: 'post',
+                body: JSON.stringify(options),
+                headers: { 'Content-Type': 'application/json', 'Authorization': 'Bearer ' + accessToken },
+            })
+            .then(res => res.json())
+            .then(json => {
+                res.send({ "result": json })
+            });
+
+
+    });
+})
